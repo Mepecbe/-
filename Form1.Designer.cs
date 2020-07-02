344f0f20e4aadea1aea0afef4bd85e9fdc12251d
Nikita <val0dko99@mail.ru>
2020-07-02 22:27:51 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.ActiveState = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(52, 63);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(133, 23);
+            this.metroButton1.TabIndex = 1;
+            this.metroButton1.Text = "Начать визуализацию";
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(23, 163);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(754, 325);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // ActiveState
+            // 
+            this.ActiveState.AutoSize = true;
+            this.ActiveState.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.ActiveState.Location = new System.Drawing.Point(52, 105);
+            this.ActiveState.Name = "ActiveState";
+            this.ActiveState.Size = new System.Drawing.Size(461, 25);
+            this.ActiveState.TabIndex = 3;
+            this.ActiveState.Text = "Начните визуализацию, нажав соответствующую кнопку";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 524);
+            this.Controls.Add(this.ActiveState);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.metroButton1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Визуализация алгоритма \"Код Хэмминга\"";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.ListView listView1;
+        private MetroFramework.Controls.MetroLabel ActiveState;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {            
+            for(int num = 0; num < 30; num++)
+            {
+                this.listView1.Columns.Add(num.ToString(), 30);
+            }
+        }
+    }
+}
